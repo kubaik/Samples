fbdaeee0bbaaf7adad68ff468bbb2814ad120b1b
Fábio Naspolini <fabionaspolini@gmail.com>
2019-11-10 21:08:47 -0300

    Forms

--- Angular/my-first-project/src/app/app.module.ts
+++ Angular/my-first-project/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {ProductAlertsComponent} from './components/product-alerts/product-alerts.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {CartComponent} from './components/cart/cart.component';
+import {ShippingComponent} from './components/shipping/shipping.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatSliderModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,9 +28,12 @@ import {MatButtonModule, MatSliderModule} from '@angular/material';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Angular/my-first-project/src/app/components/cart/cart.component.ts
+++ Angular/my-first-project/src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+
 import {CartService} from '../../services/cart.service';
 import {Product} from '../../models/product';
 
@@ -10,13 +12,29 @@ import {Product} from '../../models/product';
 export class CartComponent implements OnInit {
 
   items: Product[] = [];
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit() {
-    this.items = this.cartService.getItems();
+
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
