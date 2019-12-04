b1b9adf3debf3862dc8878f65dcac2d445c5cc2e
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-04 20:43:02 -0300

    Projeto exemplo de heróis

--- Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -32,4 +32,9 @@ export class HeroDetailComponent implements OnInit {
   private goBack() {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
