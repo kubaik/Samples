0e571d8e53240c2de3542ceae121dd84ecb098a3
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-24 03:48:35 -0300

    teste jdbc

---
+++ Java/jdbc/src/main/java/com/jdbc/sample/Main.java
@@ -0,0 +1,29 @@
+package com.jdbc.sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        var url = "jdbc:postgresql://localhost:5432/teste-jdbc";
+        var user = "postgres";
+        var password = "123456*";
+
+        try (var con = DriverManager.getConnection(url, user, password);
+             var st = con.createStatement();
+             var rs = st.executeQuery("select version()")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Main.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
