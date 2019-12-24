4c10c5ae8cc119fa5c51ba95a34dfb728b57715f
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-24 03:10:55 -0300

    Console app mavem

---
+++ Java/ConsoleAppMaven/src/main/java/JavaStatsEx.java
@@ -0,0 +1,11 @@
+public class JavaStatsEx {
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+        var app = new MyStatsApp();
+        app.run(args);
+    }
+}
