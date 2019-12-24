42d41b1ebbc20f6abe58e19c2a808e4746a1b626
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-24 02:27:02 -0300

    Console app

---
+++ Java/ConsoleApp/src/com/consoleapp/Main.java
@@ -0,0 +1,8 @@
+package com.consoleapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Console App Start");
+        System.out.println("Console App End");
+    }
+}
