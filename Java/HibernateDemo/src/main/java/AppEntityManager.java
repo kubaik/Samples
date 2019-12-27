667eaa03a0e152e89334ae5d3ae1de2719289387
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-27 01:53:50 -0300

    Hibernate demo List

--- Java/HibernateDemo/src/main/java/AppEntityManager.java
+++ Java/HibernateDemo/src/main/java/AppEntityManager.java
@@ -2,13 +2,49 @@ import models.Endereco;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 
 public class AppEntityManager {
     private EntityManagerFactory _entityManagerFactory;
 
     public void execute() {
         _entityManagerFactory = createEntityManagerFactory();
+        listEnderecos();
+        printOneEndereco();
         insertEndereco();
+    }
+
+    private void listEnderecos() {
+        var em = _entityManagerFactory.createEntityManager();
+        try {
+            var builder = em.getCriteriaBuilder();
+            var criteria = builder.createQuery(Endereco.class);
+            criteria.from(Endereco.class);
+            var result = em.createQuery(criteria).getResultList();
+            System.out.println("Id  | Rua           | Cidade");
+            for (var end : result) {
+                System.out.println(String.format("%s | %s | %s", end.getCodigo(), end.getRua(), end.getCidade()));
+            }
+        } finally {
+            em.close();
+        }
+        System.out.println("end of listEnderecos");
+    }
+
+    private void printOneEndereco() {
+        var em = _entityManagerFactory.createEntityManager();
+        try {
+            var end = em.createQuery("from Endereco e where e.codigo = :codigo", Endereco.class)
+                    .setParameter("codigo", 1)
+                    .getSingleResult();
+
+            System.out.println("Id  | Rua           | Cidade");
+            System.out.println(String.format("%s | %s | %s", end.getCodigo(), end.getRua(), end.getCidade()));
+        } finally {
+            em.close();
+        }
+        System.out.println("end of printOneEndereco");
     }
 
     private void insertEndereco() {
@@ -19,19 +55,19 @@ public class AppEntityManager {
         end.setEstado("SC");
         end.setCep("4419999");
 
-        var manager = _entityManagerFactory.createEntityManager();
+        var em = _entityManagerFactory.createEntityManager();
         try {
-            manager.getTransaction().begin();
+            em.getTransaction().begin();
             try {
-                manager.merge(end);
-                manager.getTransaction().commit();
+                em.merge(end);
+                em.getTransaction().commit();
             } catch (Exception e) {
                 System.out.println("Transação falhou : ");
                 e.printStackTrace();
-                manager.getTransaction().rollback();
+                em.getTransaction().rollback();
             }
         } finally {
-            manager.close();
+            em.close();
         }
     }
 
